71ef3ffaa9374a681de79b0a2419fce65157a0c8
Philippe Le Van (@plv) <philippe.levan@kitpages.fr>
2014-03-19 14:50:57 +0100

    Initialization test

---
+++ Tests/Console/ConsoleTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Kitpages\WorkflowBundle\Tests\Console;
+
+use Kitpages\WorkflowBundle\Tests\CommandTestCase;
+
+class ConsoleTest
+    extends CommandTestCase
+{
+    /**
+     * used to know if services are good initialized
+     */
+    public function testBasicConsole()
+    {
+        $client = self::createClient();
+        $output = $this->runCommand($client, "");
+        $this->assertContains('cache:clear', $output);
+    }
+}
